814588b522b9a2eab4af60180b6e2d2f0f2fb0c9
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-06-21 14:35:05 -0500

    think i fixed the badge thing

--- index.js
+++ index.js
@@ -37,26 +37,19 @@ var inquirer = require("inquirer");
 var fs = require("fs")
 inquirer.prompt(questions).then(function(data) {
     var fileName = data.title.toLowerCase().split(' ').join('') + ".md";
-    var titleEl = `#Repository Name: ${data.title}`;
-    var TOC = '\n'+`${data.title}: Table of Contents:`+'\n'+ `* Dependencies`+'\n'+ `* What the User needs to know`+'\n'+`* How to Contribute to Repo`+'\n'+`* Tests`+'\n'+`* Questions`;
-    var license = data.license;
+    var titleEl = `# Repository Name: ${data.title}`;
+    var TOC = '\n'+'\n'+`${data.title}: Table of Contents:`+'\n'+ `* Dependencies`+'\n'+ `* What the User needs to know`+'\n'+`* How to Contribute to Repo`+'\n'+`* Tests`+'\n'+`* Questions`;
+    var license = data.licenses;
     var badge = '\n'+`![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
     var dependencies = '\n'+'\n'+`Command to install dependencies: ${data.dependencies}`;
     var know = '\n'+'\n'+`What you need to know when using this repo: ${data.n2k}`;
     var contribute = '\n'+'\n'+`What you need to know when contributing to this repo: ${data.contribute}`;
-    fs.writeFile(fileName, [titleEl + badge + TOC + dependencies + know + contribute], (err) => {
+    fs.writeFile(fileName, [titleEl + badge +TOC + dependencies + know + contribute], (err) => {
         if (err) {
             console.log(err);
         } else {
-            renderLicenseBadge(data.license)
+            console.log(license);
             console.log("Your README has been generated.");
         }
     });
   });
-  function renderLicenseBadge(license) {
-    if (license !== "None") {
-    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
-    }
-    return ''
-   }
-  
